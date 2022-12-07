d152d1fc7fffc4718521cc6a70b0c3ffdcfbab3f
alessandro <alessandro.bampa1999@gmailcom>
2022-12-07 19:04:28 +0100

    fisrt configuration

---
+++ src/test/java/bi/testApplication/TestApplicationIT.java
@@ -0,0 +1,9 @@
+package bi.testApplication;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+
+@QuarkusIntegrationTest
+public class TestApplicationIT extends TestApplicationTest {
+    // Execute the same tests but in packaged mode.
+}
---
+++ src/test/java/bi/testApplication/TestApplicationTest.java
@@ -0,0 +1,21 @@
+package bi.testApplication;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class TestApplicationTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/api")
+          .then()
+             .statusCode(200)
+             .body(is("Hello from RESTEasy Reactive"));
+    }
+
+}
